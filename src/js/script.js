2f29597c67534f642f216a6cb099bc119bf3fc42
ortamfag <a_zhidov@outlook.com>
2022-04-02 16:01:23 +0300

    ADD: start usa/alaska

--- src/js/script.js
+++ src/js/script.js
@@ -14,15 +14,24 @@ switch2.addEventListener('click', () => trendText2.classList.toggle('trend__text
 switch2.addEventListener('click', () => trendText1.classList.remove('trend__text-active'))
 
 const britainPreview = document.querySelector('#britain__preview')
+const chinaPreview = document.querySelector('#china__preview')
+const usaPreview = document.querySelector('#usa__preview')
+const alaskaPreview = document.querySelector('#alaska__preview')
+const singaporePreview = document.querySelector('#singapore__preview')
+const koreaPreview = document.querySelector('#korea__preview')
+const oaePreview = document.querySelector('#oae__preview')
 
 const china = document.querySelector('#china')
 const chinaActive = document.querySelector('#chinaActive')
+const chinaArrow = document.querySelector('#chinaArrow-black')
 
 const usa = document.querySelector('#usa')
 const usaActive = document.querySelector('#usaActive')
+const usaArrow = document.querySelector('#usaArrow-black')
 
 const alaska = document.querySelector('#alaska')
 const alaskaActive = document.querySelector('#alaskaActive')
+const alaskaArrow = document.querySelector('#alaskaArrow-black')
 
 const britain = document.querySelector('#britain')
 const britainActive = document.querySelector('#britainActive')
@@ -30,19 +39,36 @@ const britainArrow = document.querySelector('#britainArrow-black')
 
 const singapore = document.querySelector('#singapore')
 const singaporeActive = document.querySelector('#singaporeActive')
+const singaporeArrow = document.querySelector('#singaporeArrow-black')
 
 const korea = document.querySelector('#korea')
 const koreaActive = document.querySelector('#koreaActive')
+const koreaArrow = document.querySelector('#koreaArrow-black')
 
 const oae = document.querySelector('#oae')
 const oaeActive = document.querySelector('#oaeActive')
+const oaeArrow = document.querySelector('#oaeArrow-black')
 
 
 //chinaActive
 china.addEventListener('mouseenter', () => china.classList.toggle('none'))
 china.addEventListener('mouseenter', () => chinaActive.classList.remove('none'))
+china.addEventListener('mouseenter', () => chinaArrow.classList.remove('none'))
+china.addEventListener('mouseenter', () => chinaPreview.classList.toggle('country-preview-active'))
+chinaPreview.addEventListener('mouseenter', () => china.classList.toggle('none'))
+chinaPreview.addEventListener('mouseenter', () => chinaActive.classList.remove('none'))
+chinaPreview.addEventListener('mouseenter', () => chinaArrow.classList.remove('none'))
+chinaPreview.addEventListener('mouseenter', () => chinaPreview.classList.toggle('country-preview-active'))
+
+
 chinaActive.addEventListener('mouseout', () => china.classList.remove('none'))
 chinaActive.addEventListener('mouseout', () => chinaActive.classList.toggle('none'))
+chinaActive.addEventListener('mouseout', () => chinaArrow.classList.toggle('none'))
+chinaActive.addEventListener('mouseout', () => chinaPreview.classList.remove('country-preview-active'))
+chinaPreview.addEventListener('mouseout', () => china.classList.remove('none'))
+chinaPreview.addEventListener('mouseout', () => chinaActive.classList.toggle('none'))
+chinaPreview.addEventListener('mouseout', () => chinaArrow.classList.toggle('none'))
+chinaPreview.addEventListener('mouseout', () => chinaPreview.classList.remove('country-preview-active'))
 
 //usaActive & alaska
 //usa
@@ -93,19 +119,60 @@ britainPreview.addEventListener('mouseout', () => britainPreview.classList.remov
 
 singapore.addEventListener('mouseenter', () => singapore.classList.toggle('none'))
 singapore.addEventListener('mouseenter', () => singaporeActive.classList.remove('none'))
+singapore.addEventListener('mouseenter', () => singaporeArrow.classList.remove('none'))
+singapore.addEventListener('mouseenter', () => singaporePreview.classList.toggle('country-preview-active'))
+singaporePreview.addEventListener('mouseenter', () => singapore.classList.toggle('none'))
+singaporePreview.addEventListener('mouseenter', () => singaporeActive.classList.remove('none'))
+singaporePreview.addEventListener('mouseenter', () => singaporeArrow.classList.remove('none'))
+singaporePreview.addEventListener('mouseenter', () => singaporePreview.classList.toggle('country-preview-active'))
+
+
 singaporeActive.addEventListener('mouseout', () => singapore.classList.remove('none'))
 singaporeActive.addEventListener('mouseout', () => singaporeActive.classList.toggle('none'))
+singaporeActive.addEventListener('mouseout', () => singaporeArrow.classList.toggle('none'))
+singaporeActive.addEventListener('mouseout', () => singaporePreview.classList.remove('country-preview-active'))
+singaporePreview.addEventListener('mouseout', () => singapore.classList.remove('none'))
+singaporePreview.addEventListener('mouseout', () => singaporeActive.classList.toggle('none'))
+singaporePreview.addEventListener('mouseout', () => singaporeArrow.classList.toggle('none'))
+singaporePreview.addEventListener('mouseout', () => singaporePreview.classList.remove('country-preview-active'))
 
 //korea
 
 korea.addEventListener('mouseenter', () => korea.classList.toggle('none'))
 korea.addEventListener('mouseenter', () => koreaActive.classList.remove('none'))
+korea.addEventListener('mouseenter', () => koreaArrow.classList.remove('none'))
+korea.addEventListener('mouseenter', () => koreaPreview.classList.toggle('country-preview-active'))
+koreaPreview.addEventListener('mouseenter', () => korea.classList.toggle('none'))
+koreaPreview.addEventListener('mouseenter', () => koreaActive.classList.remove('none'))
+koreaPreview.addEventListener('mouseenter', () => koreaArrow.classList.remove('none'))
+koreaPreview.addEventListener('mouseenter', () => koreaPreview.classList.toggle('country-preview-active'))
+
+
 koreaActive.addEventListener('mouseout', () => korea.classList.remove('none'))
 koreaActive.addEventListener('mouseout', () => koreaActive.classList.toggle('none'))
+koreaActive.addEventListener('mouseout', () => koreaArrow.classList.toggle('none'))
+koreaActive.addEventListener('mouseout', () => koreaPreview.classList.remove('country-preview-active'))
+koreaPreview.addEventListener('mouseout', () => korea.classList.remove('none'))
+koreaPreview.addEventListener('mouseout', () => koreaActive.classList.toggle('none'))
+koreaPreview.addEventListener('mouseout', () => koreaArrow.classList.toggle('none'))
+koreaPreview.addEventListener('mouseout', () => koreaPreview.classList.remove('country-preview-active'))
 
 //oae
 
 oae.addEventListener('mouseenter', () => oae.classList.toggle('none'))
 oae.addEventListener('mouseenter', () => oaeActive.classList.remove('none'))
+oae.addEventListener('mouseenter', () => oaeArrow.classList.remove('none'))
+oae.addEventListener('mouseenter', () => oaePreview.classList.toggle('country-preview-active'))
+oaePreview.addEventListener('mouseenter', () => oae.classList.toggle('none'))
+oaePreview.addEventListener('mouseenter', () => oaeActive.classList.remove('none'))
+oaePreview.addEventListener('mouseenter', () => oaeArrow.classList.remove('none'))
+oaePreview.addEventListener('mouseenter', () => oaePreview.classList.toggle('country-preview-active'))
+
 oaeActive.addEventListener('mouseout', () => oae.classList.remove('none'))
 oaeActive.addEventListener('mouseout', () => oaeActive.classList.toggle('none'))
+oaeActive.addEventListener('mouseout', () => oaeArrow.classList.toggle('none'))
+oaeActive.addEventListener('mouseout', () => oaePreview.classList.remove('country-preview-active'))
+oaePreview.addEventListener('mouseout', () => oae.classList.remove('none'))
+oaePreview.addEventListener('mouseout', () => oaeActive.classList.toggle('none'))
+oaePreview.addEventListener('mouseout', () => oaeArrow.classList.toggle('none'))
+oaePreview.addEventListener('mouseout', () => oaePreview.classList.remove('country-preview-active'))
