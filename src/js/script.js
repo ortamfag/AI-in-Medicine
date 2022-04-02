b5d1294cfaf4433f0979d053bb97b65b66b9158c
ortamfag <a_zhidov@outlook.com>
2022-04-02 13:49:25 +0300

    ADD: map hover

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,83 @@ switch2.addEventListener('click', () => switch2.classList.toggle('trend__switch-
 switch2.addEventListener('click', () => switch1.classList.remove('trend__switch-item-active'))
 switch2.addEventListener('click', () => trendText2.classList.toggle('trend__text-active'))
 switch2.addEventListener('click', () => trendText1.classList.remove('trend__text-active'))
+
+const china = document.querySelector('#china')
+const chinaActive = document.querySelector('#chinaActive')
+
+const usa = document.querySelector('#usa')
+const usaActive = document.querySelector('#usaActive')
+
+const alaska = document.querySelector('#alaska')
+const alaskaActive = document.querySelector('#alaskaActive')
+
+const britain = document.querySelector('#britain')
+const britainActive = document.querySelector('#britainActive')
+
+const singapore = document.querySelector('#singapore')
+const singaporeActive = document.querySelector('#singaporeActive')
+
+const korea = document.querySelector('#korea')
+const koreaActive = document.querySelector('#koreaActive')
+
+const oae = document.querySelector('#oae')
+const oaeActive = document.querySelector('#oaeActive')
+
+
+//chinaActive
+china.addEventListener('mouseenter', () => china.classList.toggle('none'))
+china.addEventListener('mouseenter', () => chinaActive.classList.remove('none'))
+chinaActive.addEventListener('mouseout', () => china.classList.remove('none'))
+chinaActive.addEventListener('mouseout', () => chinaActive.classList.toggle('none'))
+
+//usaActive & alaska
+//usa
+usa.addEventListener('mouseenter', () => usa.classList.toggle('none'))
+usa.addEventListener('mouseenter', () => alaska.classList.toggle('none'))
+usa.addEventListener('mouseenter', () => usaActive.classList.remove('none'))
+usa.addEventListener('mouseenter', () => alaskaActive.classList.remove('none'))
+
+usaActive.addEventListener('mouseout', () => usa.classList.remove('none'))
+usaActive.addEventListener('mouseout', () => alaska.classList.remove('none'))
+usaActive.addEventListener('mouseout', () => usaActive.classList.toggle('none'))
+usaActive.addEventListener('mouseout', () => alaskaActive.classList.toggle('none'))
+
+//alaska
+
+alaska.addEventListener('mouseenter', () => usa.classList.toggle('none'))
+alaska.addEventListener('mouseenter', () => alaska.classList.toggle('none'))
+alaska.addEventListener('mouseenter', () => usaActive.classList.remove('none'))
+alaska.addEventListener('mouseenter', () => alaskaActive.classList.remove('none'))
+
+alaskaActive.addEventListener('mouseout', () => usa.classList.remove('none'))
+alaskaActive.addEventListener('mouseout', () => alaska.classList.remove('none'))
+alaskaActive.addEventListener('mouseout', () => usaActive.classList.toggle('none'))
+alaskaActive.addEventListener('mouseout', () => alaskaActive.classList.toggle('none'))
+
+//britain
+
+britain.addEventListener('mouseenter', () => britain.classList.toggle('none'))
+britain.addEventListener('mouseenter', () => britainActive.classList.remove('none'))
+britainActive.addEventListener('mouseout', () => britain.classList.remove('none'))
+britainActive.addEventListener('mouseout', () => britainActive.classList.toggle('none'))
+
+//singapore
+
+singapore.addEventListener('mouseenter', () => singapore.classList.toggle('none'))
+singapore.addEventListener('mouseenter', () => singaporeActive.classList.remove('none'))
+singaporeActive.addEventListener('mouseout', () => singapore.classList.remove('none'))
+singaporeActive.addEventListener('mouseout', () => singaporeActive.classList.toggle('none'))
+
+//korea
+
+korea.addEventListener('mouseenter', () => korea.classList.toggle('none'))
+korea.addEventListener('mouseenter', () => koreaActive.classList.remove('none'))
+koreaActive.addEventListener('mouseout', () => korea.classList.remove('none'))
+koreaActive.addEventListener('mouseout', () => koreaActive.classList.toggle('none'))
+
+//oae
+
+oae.addEventListener('mouseenter', () => oae.classList.toggle('none'))
+oae.addEventListener('mouseenter', () => oaeActive.classList.remove('none'))
+oaeActive.addEventListener('mouseout', () => oae.classList.remove('none'))
+oaeActive.addEventListener('mouseout', () => oaeActive.classList.toggle('none'))
